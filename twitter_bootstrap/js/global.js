c681e1398c4e368fa545987c528f90c4d789495b
Juan Ramon <juanramon.diaz@gmail.com>
2011-10-18 18:09:57 +0200

    deleted commented javascript code

--- twitter_bootstrap/js/global.js
+++ twitter_bootstrap/js/global.js
@@ -37,37 +37,6 @@ $(document).ready(function(){
     // Select with choosen
     // ===============================
     $(".chzn-select").chosen();
-	/*// User_menu show/hide submenu
-	$("#user_menu .with_sub").hover(function(){
-		$(this).find("ul").show();
-	},
-	function(){
-		$(this).find("ul").hide();
-	});
-	
-	// Flash messages effect
-	$("#FlashMessage").slideDown('slow').delay(3000).slideUp('slow');
-    
-	// Open login box in situ
-        $('#login_open').click(function(e) {
-            e.preventDefault();
-            $('#login').slideToggle('slow', function(){});
-        });
-
-	
-	// Show advanced search in internal pages
-	$("#expand_advanced").click(function(e){
-		e.preventDefault();
-		$(".search .extras").slideToggle();
-	});
-	
-	// Show/hide Report as 
-	$("#report").hover(function(){
-		$(this).find("span").show();
-	},
-	function(){
-		$(this).find("span").hide();
-	});*/
 });
 
 function text_validation (element) {
